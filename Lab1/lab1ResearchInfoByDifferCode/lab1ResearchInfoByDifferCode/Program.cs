82042a4e488cbad558d76806b57b159f60904a79
lizakrishen <miss.krishen80@gmail.com>
2018-04-17 11:56:35 +0300

    changes

---
+++ Lab1/lab1ResearchInfoByDifferCode/lab1ResearchInfoByDifferCode/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab1ResearchInfoByDifferCoding
+{
+    public class Program
+    {
+        public static Tuple<SortedDictionary<char, int>, SortedDictionary<char, double>, double, double, int> FindAmountLetters(string text)
+        {
+            Regex escapeMark = new Regex(@"\s");
+            text = escapeMark.Replace(text.ToString(), "");
+
+            SortedDictionary<char, int> alphabet = new SortedDictionary<char, int>();
+            SortedDictionary<char, double> frequency = new SortedDictionary<char, double>();
+
+            foreach (char c in text)
+                if (alphabet.ContainsKey(c/*Char.ToLower(c)*/))
+                    alphabet[c/*Char.ToLower(c)*/]++;
+                else
+                    alphabet.Add(c/*Char.ToLower(c)*/, 1);
+
+            int howManyLetters = alphabet.Values.Sum();
+
+            foreach (char c in alphabet.Keys)
+                frequency.Add(c, alphabet[c] * 1.0 / howManyLetters);
+
+            double middleEntropy = 0;
+            double log;
+            foreach (char c in frequency.Keys)
+            {
+                log = Math.Log(frequency[c], 2);
+                if (log != double.NaN && !double.IsInfinity(log))
+                    middleEntropy -= frequency[c] * log;
+            }
+
+            double amountInfoInText = middleEntropy * howManyLetters;
+            return Tuple.Create(alphabet, frequency, middleEntropy, amountInfoInText, howManyLetters);
+        }
+        public static void Output(string path, bool printFrequency = true)
+        {
+            var i = FindAmountLetters(File.ReadAllText(path));
+            Console.WriteLine("Середня ентропія алфавіту для цього тексту = {0} bits", i.Item3);
+            Console.WriteLine("Кількість символів: {0}", i.Item5);
+            Console.WriteLine("Кількість інформації в тексті {0} bytes", i.Item4 / 8);
+            Console.WriteLine("Частоту кожного символу: ");
+            if (printFrequency)
+                foreach (char c in i.Item2.Keys)
+                    Console.WriteLine("{0} = {1:0.00000}", c, i.Item2[c]);
+        }
+        static void Main(string[] args)
+        {
+            string file1 = "Мені тринадцятий минало.txt",
+                file2 = "Казка про ріпку.txt",
+                file3 = "PCI.txt",
+                path = @"D:\6sem\computer_systems\Lab1\lab1files\";
+            Console.OutputEncoding = Encoding.GetEncoding(1200);
+
+            Output(path + file1);
+            Output(path + file2);
+            Output(path + file3); Console.ReadKey();
+        }
+    }
+}
+
+
